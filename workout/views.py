289d4b5c669bb85401a7a4bed6e31bcfda52e081
Kat Kovacs <kovacskatalinkisvad@gmail.com>
2023-11-05 12:55:51 +0000

    Testing webhook

--- workout/views.py
+++ workout/views.py
@@ -17,7 +17,7 @@ def update(request):
         Here the name of my directory is "test.pythonanywhere.com"
         """
     if request.method == "POST":
-        repo = git.Repo("kkovacs.pythonanywhere.com/workout")
+        repo = git.Repo("git@github.com:kisvad/workout.git")
         origin = repo.remotes.origin
 
         origin.pull()
