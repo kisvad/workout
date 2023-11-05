3734a2acf84df6dfeb4c3e1874fa74d8c6834983
Kat Kovacs <kovacskatalinkisvad@gmail.com>
2023-11-05 13:37:47 +0000

    typo

--- workout/views.py
+++ workout/views.py
@@ -17,7 +17,7 @@ def update(request):
         Here the name of my directory is "test.pythonanywhere.com"
         """
     if request.method == "POST":
-        repo = git.Repo("https://kkovacs.pythonanywhere.com/workout.git")
+        repo = git.Repo("kkovacs.pythonanywhere.com/workout.git")
         origin = repo.remotes.origin
 
         origin.pull()
