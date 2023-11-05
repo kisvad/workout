787ba90dd4a9551333fe1494e14fd54492af87c4
Kat Kovacs <kovacskatalinkisvad@gmail.com>
2023-11-05 14:57:39 +0000

    Testion webhook

--- workout/views.py
+++ workout/views.py
@@ -1,8 +1,14 @@
 """Module providing the main views."""
+import hashlib
+import hmac
+import os
+from http.client import HTTPException
 import git
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from django.views.decorators.csrf import csrf_exempt
+
+w_key = os.environ['WEBHOOK_SECRET']
 
 class HomePage(TemplateView):
     """Class representing a person"""
@@ -17,6 +23,9 @@ def update(request):
         Here the name of my directory is "test.pythonanywhere.com"
         """
     if request.method == "POST":
+        x_hub_signature = request.headers.get('x-hub-signature-256')
+        verify_signature(request.body(), w_key, x_hub_signature)
+        
         repo = git.Repo("./workout")
         origin = repo.remotes.origin
 
@@ -25,3 +34,20 @@ def update(request):
         return HttpResponse("Updated code on PythonAnywhere")
     else:
         return HttpResponse("Couldn't update the code on PythonAnywhere")
+    
+def verify_signature(payload_body, secret_token, signature_header):
+    """Verify that the payload was sent from GitHub by validating SHA256.
+
+    Raise and return 403 if not authorized.
+
+    Args:
+        payload_body: original request body to verify (request.body())
+        secret_token: GitHub app webhook token (WEBHOOK_SECRET)
+        signature_header: header received from GitHub (x-hub-signature-256)
+    """
+    if not signature_header:
+        raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
+    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    expected_signature = "sha256=" + hash_object.hexdigest()
+    if not hmac.compare_digest(expected_signature, signature_header):
+        raise HTTPException(status_code=403, detail="Request signatures didn't match!")
